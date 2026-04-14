7f0fd82c6074a704689a836e600ba0fe2787dd9f
Omwansam <omwansamarnold@gmail.com>
2026-04-14 09:43:14 +0300

    delete env files

--- BACKEND/utils/project-image-url.util.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-const buildStoredImagePath = (filename) => {
-  if (!filename) return null;
-  const normalized = filename.split(path.sep).join("/");
-  return `/uploads/projects/${normalized}`;
-};
-
-module.exports = { buildStoredImagePath };
